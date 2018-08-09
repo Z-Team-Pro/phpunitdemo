c384c3790646a4783b1060eb268095989167d14d
Ramy Hakam <pencilsoft1@gmail.com>
2018-08-09 13:36:17 +0200

    depends tests

--- ArrayTest.php
+++ ArrayTest.php
@@ -5,18 +5,41 @@ use PHPUnit\Framework\TestCase;
 
 class ArrayTest extends  TestCase{
 
+   //depends function on function
 
-    public  function testPushAndPop(){
+    public  function  testEmpty(){
 
         $stack=[];
-        $this->assertSame(0,count($stack));
+        $this->assertEmpty($stack);
+        return $stack;
+
+    }
+    /**
+     * @depends testEmpty
+     */
+
+
+    public  function testPush(array  $stack){
+
 
         array_push($stack, 'foo');
         $this->assertSame('foo', $stack[count($stack)-1]);
         $this->assertSame(1, count($stack));
-        $this->assertSame('foo', array_pop($stack));
-        $this->assertSame(0, count($stack));
+        return $stack;
     }
+
+    /**
+     * @depends testPush
+     */
+
+    public function testPop (array $stack){
+
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertEmpty($stack);
+
+    }
+
+
 
 
 }
